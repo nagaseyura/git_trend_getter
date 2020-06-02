d687e19ed6cf2a74ab6eeea000cab38081fdda89
EC2 Default User <ec2-user@ip-172-31-44-169.ap-northeast-1.compute.internal>
2020-06-02 03:01:32 +0000

    画像編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,6 @@ class UsersController < ApplicationController
   def show
     user = User.find(params[:id])
     @nickname = user.nickname
-    @tweets = user.tweets.order("created_at DESC").page(params[:page]).per(9)
+    @tweets = user.tweets.order("created_at DESC").page(params[:page]).per(10)
   end 
 end
